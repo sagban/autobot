91ed6cc82f1ba76a43e6814e560aaf3859024d96
Sagar Bansal <sagarbansal099@gmail.com>
2018-03-23 18:09:03 +0530

    added adminapps and clubapps

--- autobot/settings.py
+++ autobot/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'botapp.apps.AdminappConfig',
+    'botapp.apps.UserappConfig',
     'botapp.apps.BotappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
