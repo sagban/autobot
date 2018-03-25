d1f794f8d17d6c583fa3c08db931da77bbf1af8f
Sagar Bansal <sagarbansal099@gmail.com>
2018-03-25 19:37:07 +0530

    server validation login

--- autobot/urls.py
+++ autobot/urls.py
@@ -22,5 +22,6 @@ from userapp import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bot_views.index),
-    path('login/', admin_views.login),
+    path('admin-login/', admin_views.adminLogin),
+    path('admin-validate/', admin_views.adminLoginValidate),
 ]
