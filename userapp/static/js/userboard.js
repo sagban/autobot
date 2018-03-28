dbd8b99d656eba15f3444e7ef02e70184fa0bd9b
priyankjpi07-sopho <priyankjpi07@gmail.com>
2018-03-28 11:18:50 +0530

    added static files

---
+++ userapp/static/js/userboard.js
@@ -0,0 +1,9 @@
+
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, options);
+
+  /*// Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });*/
