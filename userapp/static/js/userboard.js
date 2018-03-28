503dcdfbbc4526ca3ab6e72616939e6bd1178293
priyankjpi07-sopho <priyankjpi07@gmail.com>
2018-03-28 15:18:48 +0530

    user dashboard | minor fixes

--- userapp/static/js/userboard.js
+++
@@ -1,9 +0,0 @@
-
-  var elem = document.querySelector('.sidenav');
-  var instance = M.Sidenav.init(elem, options);
-
-  /*// Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });*/
