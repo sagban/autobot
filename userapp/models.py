43fae585576a0879c8432c31bc02e98bb80f4515
priyankjpi07-sopho <priyankjpi07@gmail.com>
2018-03-30 11:34:27 +0530

    models updated

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userID = models.AutoField(primary_key=True)
+    userName = models.TextField(max_length= 80)
+    userUID = models.TextField(max_length=14)
+    userPass = models.TextField(null=False, max_length=15)
+    userDOB = models.DateField()
+    userGender = models.TextField(max_length=6)
+    userMail = models.EmailField()
+    userAddress = models.TextField(max_length=1000)
+    userLicense = models.TextField(max_length=15)
+
+    def __str__(self):
+        return self.userUID
+
+class Vehicles(models.Model):
+    vehicleID = models.AutoField(primary_key=True)
+    engineNumber = models.TextField(max_length=17)
+    chassisNumber = models.TextField(max_length=17)
+    fuelType = models.TextField(max_length=20)
+    makerModel = models.TextField(max_length=20)
+    makerClass = models.TextField(max_length=20)
+    regDate = models.DateField()
+    regAuth = models.TextField(max_length=20)
+
+    def __str__(self):
+        return self.vehicleID
+
+class UserVehicleOwnership(models.Model):
+    UserVehicleID = models.AutoField(primary_key=True)
+    userID = models.ForeignKey(User, null=False)
+    vehicleID = models.ForeignKey(Vehicles, null=False)
