057b8c810ecc5c2b3438ae8188000e59e3ad250e
priyankjpi07-sopho <priyankjpi07@gmail.com>
2018-03-31 00:10:52 +0530

    admin register models

--- userapp/admin.py
+++ userapp/admin.py
@@ -3,3 +3,6 @@ from userapp.models import *
 
 # Register your models here.
 admin.site.register(user)
+admin.site.register(vehicle)
+admin.site.register(userVehicleOwnership)
+admin.site.register(userRegister)
